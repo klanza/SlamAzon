4e304da1c4a8e0d9669269fe70b1941901bc25aa
klanza <klanza91@gmail.com>
2018-02-01 01:31:54 -0700

    Created promise to prevent table from loading incorrectly, beginning inquirer prompt

--- slamazonCustomer.js
+++ slamazonCustomer.js
@@ -4,6 +4,7 @@ const {table} = require('table');
 
 const pw = require('./password.js');
 
+// Connection information for mySQL server
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
@@ -12,28 +13,69 @@ const connection = mysql.createConnection({
     database: 'slamazon_db',
 });
 
-const inventoryTable = () => {
-    connection.query('SELECT * FROM products', function(err, res) {
-        if (err) throw err;
-        let arrayTable = [];
-        res.forEach((element) => {
-            let arrayRow = [];
-            arrayRow.push(element.item_id);
-            arrayRow.push(element.product_name);
-            arrayRow.push(element.department_name);
-            arrayRow.push(element.price);
-            arrayRow.push(element.stock_quantity);
-            arrayTable.push(arrayRow);
+// Inquirer prompt questions
+const purchaseQuestions = [
+    {
+        type: 'input',
+        name: 'itemID',
+        message: 'What is the ID of the item you would like to purchase?',
+        validate: function(value) {
+            const pass = value.match(/(^\d{3})/g);
+            if (pass) {
+                return true;
+            };
+        },
+    },
+    {
+        type: 'input',
+        name: 'quantity',
+        message: 'Please enter the quantity of items you would like to purchase:',
+        validate: function(value) {
+            const pass = value.match(/\d/g);
+            if (pass) {
+                return true;
+            };
+            return 'Please enter a valid 3 digit item ID.';
+        },
+    },
+];
+
+// Query databse to grab all products
+const queryDatabase = () => {
+    return new Promise((resolve, reject) => {
+        connection.query('SELECT * FROM products', function(err, res) {
+            if (err) throw err;
+            resolve(res)
         });
-        // NPM package utility to create table from arrays
-        let output = table(arrayTable);
-        console.log(output);
-        connection.end();
     });
+};
+
+// Create table using 'table' npm package
+const inventoryTable = (res) => {
+    let arrayTable = [];
+    res.forEach((element) => {
+        let arrayRow = [];
+        arrayRow.push(element.item_id);
+        arrayRow.push(element.product_name);
+        arrayRow.push(element.department_name);
+        arrayRow.push(element.price);
+        arrayRow.push(element.stock_quantity);
+        arrayTable.push(arrayRow);
+    });
+    // NPM package utility to create table from arrays
+    let output = table(arrayTable);
+    console.log(output);
 };
 
 connection.connect(function(err) {
     if (err) throw err;
-    console.log('Connected as id: ' + connection.threadId);
-    inventoryTable();
+    console.log('===============================================================');
+    console.log('=============== COME ON AND SLAM WITH SLAMAZON ================');
+    console.log('===============================================================');
+    queryDatabase().then((response) => {
+        inventoryTable(response);
+        inquirer.prompt(questions).then((answers) {
+
+        });
+    });
 });
