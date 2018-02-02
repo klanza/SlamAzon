8f3578e5c5eec7ee2f5c2cc32e47a3825fcfc1f8
klanza <klanza91@gmail.com>
2018-02-01 18:12:45 -0700

    Completed functionality for slamazonCustomer.js

--- slamazonCustomer.js
+++ slamazonCustomer.js
@@ -13,17 +13,18 @@ const connection = mysql.createConnection({
     database: 'slamazon_db',
 });
 
-// Questions used in 'inquirer' npm package
+// Questions and functions used in 'inquirer' npm package
 const purchaseQuestions = [
     {
         type: 'input',
         name: 'itemID',
         message: 'What is the ID of the item you would like to purchase?',
         validate: function(value) {
-            const pass = value.match(/(^\d{3})/g);
+            const pass = value.match(/(^\d{3}$)/g);
             if (pass) {
                 return true;
             };
+            return 'Please enter a valid 3 digit item ID.';
         },
     },
     {
@@ -35,7 +36,7 @@ const purchaseQuestions = [
             if (pass) {
                 return true;
             };
-            return 'Please enter a valid 3 digit item ID.';
+            return 'Please enter a valid number for quantity.';
         },
     },
 ];
@@ -45,25 +46,52 @@ const queryDatabase = () => {
     return new Promise((resolve, reject) => {
         connection.query('SELECT * FROM products', function(err, res) {
             if (err) throw err;
-            resolve(res)
+            resolve(res);
         });
     });
 };
 
-const queryRow = (answers) => {
+const inventoryQuery = (answers) => {
     let query = 'SELECT stock_quantity, product_name, price FROM products WHERE ?';
     connection.query(query, {item_id: answers.itemID}, function(err, res) {
-        if (parseInt(answers.quantity) <= parseInt(res[0].stock_quantity)) {
-            console.log('Success');
+        if (res[0] === undefined) {
+            console.log('Sorry, your transaction could not be completed: Incorrect ID');
+            reloadApplicationFailure();
+        } else if (parseInt(answers.quantity) <= parseInt(res[0].stock_quantity)) {
             inquirer.prompt({
                 type: 'confirm',
-                name: 'confirm',
+                name: 'confirmOrder',
                 message: `Confirm your order: Purchasing ${answers.quantity} units of ${res[0].product_name}?`
             }).then((answer) => {
-                console.log('yay');
+                console.log(answer);
+                console.log(res[0].stock_quantity);
+                updateInventory(res[0].stock_quantity, answers.quantity, answers.itemID);
             });
+        } else {
+            console.log('Sorry, your transaction could not be completed: Insufficient quantity');
+            reloadApplicationFailure();
         }
     });
+};
+
+const updateInventory = (inStock, orderQuantity, idNumber) => {
+    let updatedQuantity = (inStock - orderQuantity);
+    connection.query(
+        'UPDATE products SET ? WHERE ?',
+        [
+            {
+                stock_quantity: updatedQuantity,
+            },
+            {
+                item_id: idNumber,
+            },
+        ],
+        function(err, res) {
+            console.log('We have completed your transaction!');
+            console.log('Thank you for Space Jamming with Slamazon!');
+            reloadApplicationSuccess();
+        }
+    );
 };
 
 // Create table using 'table' npm package
@@ -83,16 +111,49 @@ const inventoryTable = (res) => {
     console.log(output);
 };
 
-connection.connect(function(err) {
-    if (err) throw err;
+// Reload application if application fails
+const reloadApplicationFailure = () => {
+    inquirer.prompt({
+        type: 'confirm',
+        name: 'reload',
+        message: 'We could not complete your transaction, would you like to try again?',
+    }).then((answer) => {
+        if (answer.reload === true) {
+            loadStorefront();
+        } else {
+            connection.end();
+        }
+    });
+};
+
+// Reload application if application fails
+const reloadApplicationSuccess = () => {
+    inquirer.prompt({
+        type: 'confirm',
+        name: 'reload',
+        message: 'Would you like to make another purchase?',
+    }).then((answer) => {
+        if (answer.reload === true) {
+            loadStorefront();
+        } else {
+            connection.end();
+        }
+    });
+};
+
+
+const loadStorefront = () => connection.connect(function(err) {
+    // if (err) throw err;
     console.log('===============================================================');
     console.log('=============== COME ON AND SLAM WITH SLAMAZON ================');
     console.log('===============================================================');
     queryDatabase().then((response) => {
         inventoryTable(response);
         inquirer.prompt(purchaseQuestions).then((answers) => {
-            queryRow(answers);
-            connection.end();
+            inventoryQuery(answers);
         });
     });
 });
+
+// Initialize application
+loadStorefront();
